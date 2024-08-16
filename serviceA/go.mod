15e88bdf83ad2a989c4e85e6e04fbe0612cd2a2d
Elton Minetto <elton.minetto@picpay.com>
2024-08-16 17:54:56 -0300

    feat: add circuit breaker

--- serviceA/go.mod
+++ serviceA/go.mod
@@ -3,6 +3,7 @@ module github.com/eminetto/post-failsafe-go/sericeA
 go 1.22.5
 
 require (
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
 	github.com/failsafe-go/failsafe-go v0.6.7 // indirect
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
 )
--- serviceA/main.go
+++ serviceA/main.go
@@ -7,9 +7,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/failsafe-go/failsafe-go"
+	"github.com/failsafe-go/failsafe-go/circuitbreaker"
 	"github.com/failsafe-go/failsafe-go/failsafehttp"
-	"github.com/failsafe-go/failsafe-go/retrypolicy"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -21,21 +20,35 @@ func main() {
 		type response struct {
 			Message string `json:"message"`
 		}
-		// Create a RetryPolicy that only handles 500 responses, with backoff delays between retries
-		retryPolicy := retrypolicy.Builder[*http.Response]().
-			HandleIf(func(response *http.Response, _ error) bool {
-				return response != nil && response.StatusCode == 500
+		// Create a CircuitBreaker that handles 503 responses and uses a half-open delay based on the Retry-After header
+		circuitBreaker := circuitbreaker.Builder[*http.Response]().
+			HandleIf(func(response *http.Response, err error) bool {
+				return response != nil && response.StatusCode == http.StatusServiceUnavailable
 			}).
-			WithBackoff(time.Second, 10*time.Second).
-			OnRetryScheduled(func(e failsafe.ExecutionScheduledEvent[*http.Response]) {
-				fmt.Println("Retry", e.Attempts(), "after delay of", e.Delay)
-			}).Build()
+			WithDelayFunc(failsafehttp.DelayFunc).
+			OnStateChanged(func(event circuitbreaker.StateChangedEvent) {
+				fmt.Println("circuit breaker state changed", event)
+			}).
+			Build()
 
 		// Use the RetryPolicy with a failsafe RoundTripper
-		roundTripper := failsafehttp.NewRoundTripper(nil, retryPolicy)
+		roundTripper := failsafehttp.NewRoundTripper(nil, circuitBreaker)
 		client := &http.Client{Transport: roundTripper}
 
-		resp, err := client.Get("http://localhost:3001")
+		sendGet := func() (*http.Response, error) {
+			fmt.Println("Sending request")
+			resp, err := client.Get("http://localhost:3001")
+			return resp, err
+		}
+		maxRetries := 3
+		resp, err := sendGet()
+		for i := 0; i < maxRetries; i++ {
+			if err == nil && resp != nil && resp.StatusCode != http.StatusServiceUnavailable && resp.StatusCode != http.StatusTooManyRequests {
+				break
+			}
+			time.Sleep(circuitBreaker.RemainingDelay()) // Wait for circuit breaker's delay, provided by the Retry-After header
+			resp, err = sendGet()
+		}
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
